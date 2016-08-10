f3d4f3676310bad32d4f128025013dd9b9f9b88d
Hui Lin <lhmida@gmail.com>
2016-08-10 18:29:52 -0500

    last minor change didn't work well, reversed

--- resources/views/templates/glance/glancePage-groups.blade.php
+++ resources/views/templates/glance/glancePage-groups.blade.php
@@ -46,7 +46,7 @@
                         ------------------------------}}
                         <div class="bg--gray--lighter pA--md">
                             <div class="mB bold">
-                                {{--<span class="icon icon-rocket mR"></span>--}}
+                                <span class="icon icon-rocket mR"></span>
                                 Latest Feed
                             </div>
                             <div class="h--article--single">
